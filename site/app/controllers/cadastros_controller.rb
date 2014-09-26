1210083921e20ff68984eab81b2f4008f62ac470
Gabriel Silva <gabrielms.silva@live.com>
2014-09-26 09:29:15 -0300

    Fixes #28 Fixes #29 Fixes #30 Fixes #31 Fixes #32 Fixes #33

--- site/app/controllers/cadastros_controller.rb
+++ site/app/controllers/cadastros_controller.rb
@@ -9,16 +9,11 @@ class CadastrosController < ApplicationController
 	end
 
 	def create
-		#@cadastro = Cadastro.new(params.require(:cadastro).permit(:nome, :login, :senha))
-		#@cadastro.save
-		#redirect_to :login_index
 		@cadastro = Usuario.new
 		@cadastro.Nome = params[:nome]
 		@cadastro.username = params[:login]
 		@cadastro.senha = params[:senha]
 		@cadastro.save
-		puts 'aqui'
-		puts 'cadastro realizado'
 		redirect_to :login_index
 	end
 
