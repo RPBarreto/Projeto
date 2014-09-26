0bfda53c551e6bf13a70ce29b46cb7c2ceccc033
Gabriel Silva <gabrielms.silva@live.com>
2014-09-25 22:32:33 -0300

    correcao de algumas validacoes Fixes #23

--- site/app/models/arquivo.rb
+++ site/app/models/arquivo.rb
@@ -1,4 +1,4 @@
 class Arquivo < ActiveRecord::Base
-	 validates :descricao, presence :true
+	 validates :descricao, presence: true
 	 validates :descricao, length: {minimum : 10}
 end
--- site/app/models/turma.rb
+++ site/app/models/turma.rb
@@ -1,6 +1,6 @@
 class Turma < ActiveRecord::Base
-	validates :ano, :curso presence :true
+	validates :ano, :curso presence: true
 	validates :ano, :curso length: {minimum : 4}
-	validates :ano, format: { with: ^[A-Za-z0-9 _]*[A-Za-z0-9][A-Za-z0-9 _]*$ }
-	validates :curso, format: { with: ^[A-Za-z _]*[A-Za-z][A-Za-z]*$ }
+	validates :ano, format: { with: /[A-Za-z0-9 _]*[A-Za-z0-9][A-Za-z0-9 _]+/ }
+	validates :curso, format: { with: /[A-Za-z _]*[A-Za-z][A-Za-z]+/ }
 end
