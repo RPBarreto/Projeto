27bee311ef07281dceee313f6eb06f67ed76e683
Bruno Chim Silvera <bruno0.0cham@gmail.com>
2014-12-02 16:01:31 -0200

    correção model comentario Fixes #82

--- site/db/migrate/20141202180015_create_comentarios.rb
+++ site/db/migrate/20141202180015_create_comentarios.rb
@@ -2,7 +2,7 @@ class CreateComentarios < ActiveRecord::Migration
   def change
     create_table :comentarios do |t|
       t.integer :id_post
-      t.varchar :conteudo
+      t.string :conteudo
       t.integer :id_usuario
 
       t.timestamps
