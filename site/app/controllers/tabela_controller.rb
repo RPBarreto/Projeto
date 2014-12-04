9efd8482953e23c21699e4f4dc727e1248c210ec
Renan P. Barreto <renan.barretoo@hotmail.com>
2014-12-04 04:27:39 -0200

    Fixes #93

--- site/app/controllers/tabela_controller.rb
+++ site/app/controllers/tabela_controller.rb
@@ -4,7 +4,10 @@ class TabelaController < ApplicationController
 	end
 
 	def elemento
+		@curiosidade = Array.new 
 		@elemento = Elemento.find_by nome: params['element']
 		@massa = @elemento.num_protons + @elemento.num_neutrons
+		@where = 'elementos.nome = '+params['element']
+		@curiosidade= Curiosidade.joins(:elementos).where(@where)
 	end
 end
