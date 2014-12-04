3b7a34fa8dbdaa9ecba4b24a79ede0a95dcd18d9
Renan P. Barreto <renan.barretoo@hotmail.com>
2014-12-04 04:29:02 -0200

    Fixes #94

--- site/db/migrate/20141031014624_create_usuarios.rb
+++ site/db/migrate/20141031014624_create_usuarios.rb
@@ -2,7 +2,7 @@ class CreateUsuarios < ActiveRecord::Migration
   def change
     create_table :usuarios do |t|
       t.string :nome
-      t.string :username, limit: 10
+      t.string :username, limit: 16
       t.string :password, limit: 32
 
       t.timestamps
