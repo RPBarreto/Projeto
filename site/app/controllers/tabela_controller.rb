6a23d0148489ca787b2e66aed0b4ad1f820d28bd
Renan P. Barreto <renan.barretoo@hotmail.com>
2014-12-04 12:16:55 -0200

    Fixes #104

--- site/app/controllers/tabela_controller.rb
+++ site/app/controllers/tabela_controller.rb
@@ -4,10 +4,12 @@ class TabelaController < ApplicationController
 	end
 
 	def elemento
-		@curiosidade = Array.new 
+		#@curiosidade = Array.new 
 		@elemento = Elemento.find_by nome: params['element']
 		@massa = @elemento.num_protons + @elemento.num_neutrons
-		@where = 'elementos.nome = '+params['element']
-		@curiosidade= Curiosidade.joins(:elementos).where(@where)
+
+	#	@where = 'nome = '+params['element']
+#		@join =  ' JOIN curiosidade ON elementos.id = curiosidades.elemento'
+	#	@curiosidade= Curiosidade.joins(@join).where(@where).select('texto as field_one').first
 	end
 end
