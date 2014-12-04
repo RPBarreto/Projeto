cc032c587d373b33f3ffdf273c1703d60c156cc8
Renan P. Barreto <renan.barretoo@hotmail.com>
2014-12-04 04:10:39 -0200

    Fixes 88

--- site/app/models/usuario.rb
+++ site/app/models/usuario.rb
@@ -1,5 +1,9 @@
 class Usuario < ActiveRecord::Base
-
+	validates :nome, :username, :password, presence: true
+	validates :username, length: {maximun: 16}
+	validates :password, length: {maximun: 32}
+	validates :username, length: {minimun: 6}
+	validates :username, length: {maximun: 6}
 
 	
 end
